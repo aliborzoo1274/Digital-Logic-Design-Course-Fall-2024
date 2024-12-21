bcfffd04ca0e3654f596fad60245958780790907
Ali Borzoozadeh <aliborzoo1274@gmail.com>
2024-12-21 23:13:12 +0330

    Shift register completed.

---
+++ CA3/Shift_Register.v
@@ -0,0 +1,80 @@
+module shift_register_24 (
+    input Ser_In,
+    input [0:23] Par_In,
+    input Par_load,
+    input shift_en,
+    input rst,
+    input clk,
+    output [0:23] Par_out,
+    output Ser_Out
+);
+    wire [0:23] shift_reg;
+    genvar i;
+
+    generate
+        for (i = 0; i < 24; i = i + 1) begin : gen_shift_24
+            if (i == 0) begin
+                dff u_dff (
+                    .D(Par_load ? Par_In[i] : (shift_en ? Ser_In : shift_reg[i])),
+                    .Clock(clk),
+                    .en(shift_en | Par_load),
+                    .rst(rst),
+                    .Q(shift_reg[i])
+                );
+            end else begin
+                dff u_dff (
+                    .D(Par_load ? Par_In[i] : (shift_en ? shift_reg[i-1] : shift_reg[i])),
+                    .Clock(clk),
+                    .en(shift_en | Par_load),
+                    .rst(rst),
+                    .Q(shift_reg[i])
+                );
+            end
+        end
+    endgenerate
+
+    assign Par_out = shift_reg;
+    assign Ser_Out = shift_reg[23];
+
+endmodule
+
+
+module shift_register_80 (
+    input Ser_In,
+    input [0:79] Par_In,
+    input Par_load,
+    input shift_en,
+    input rst,
+    input clk,
+    output [0:79] Par_out,
+    output Ser_Out
+);
+    wire [0:79] shift_reg;
+    genvar i;
+
+    generate
+        for (i = 0; i < 80; i = i + 1) begin : gen_shift_80
+            if (i == 0) begin
+                dff u_dff (
+                    .D(Par_load ? Par_In[i] : (shift_en ? Ser_In : shift_reg[i])),
+                    .Clock(clk),
+                    .en(shift_en | Par_load),
+                    .rst(rst),
+                    .Q(shift_reg[i])
+                );
+            end else begin
+                dff u_dff (
+                    .D(Par_load ? Par_In[i] : (shift_en ? shift_reg[i-1] : shift_reg[i])),
+                    .Clock(clk),
+                    .en(shift_en | Par_load),
+                    .rst(rst),
+                    .Q(shift_reg[i])
+                );
+            end
+        end
+    endgenerate
+
+    assign Par_out = shift_reg;
+    assign Ser_Out = shift_reg[79];
+
+endmodule
---
+++ CA3/Shift_Register_TB.v
@@ -0,0 +1,95 @@
+`timescale 1ns/1ns
+module shift_register_24_tb();
+    reg Ser_In, Par_load, shift_en, rst, clk;
+    reg [23:0] Par_In;
+    wire [23:0] Par_out;
+    wire Ser_Out;
+
+    shift_register_24 UT (
+        .Ser_In(Ser_In),
+        .Par_In(Par_In),
+        .Par_load(Par_load),
+        .shift_en(shift_en),
+        .rst(rst),
+        .clk(clk),
+        .Par_out(Par_out),
+        .Ser_Out(Ser_Out)
+    );
+
+    initial begin
+        clk = 1'b0;
+        forever #5 clk = ~clk;
+    end
+
+    initial begin
+        Ser_In = 1'b0;
+        Par_In = 24'hA5A5A5;
+        Par_load = 1'b0;
+        shift_en = 1'b0;
+        rst = 1'b1;
+        #10 rst = 1'b0;
+
+        Par_load = 1'b1;
+        #10 Par_load = 1'b0;
+
+        shift_en = 1'b1;
+        Ser_In = 1'b1;
+        #50;
+
+        Ser_In = 1'b0;
+        #50;
+
+        rst = 1'b1;
+        #10 rst = 1'b0;
+
+        $finish;
+    end
+endmodule
+
+
+module shift_register_80_tb();
+    reg Ser_In, Par_load, shift_en, rst, clk;
+    reg [79:0] Par_In;
+    wire [79:0] Par_out;
+    wire Ser_Out;
+
+    shift_register_80 UT (
+        .Ser_In(Ser_In),
+        .Par_In(Par_In),
+        .Par_load(Par_load),
+        .shift_en(shift_en),
+        .rst(rst),
+        .clk(clk),
+        .Par_out(Par_out),
+        .Ser_Out(Ser_Out)
+    );
+
+    initial begin
+        clk = 1'b0;
+        forever #5 clk = ~clk; // 10ns clock period
+    end
+
+    initial begin
+        Ser_In = 1'b0;
+        Par_In = 80'h123456789ABCDEF01234;
+        Par_load = 1'b0;
+        shift_en = 1'b0;
+        rst = 1'b1;
+        #10 rst = 1'b0;
+
+        Par_load = 1'b1;
+        #10 Par_load = 1'b0;
+
+        shift_en = 1'b1;
+        Ser_In = 1'b1;
+        #100;
+
+        Ser_In = 1'b0;
+        #100;
+
+        rst = 1'b1;
+        #10 rst = 1'b0;
+
+        $finish;
+    end
+endmodule
